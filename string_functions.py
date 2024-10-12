f817c468c4f4528d86c47146bbf26716f0652b55
hellojul <hellojulien124@gmail.com>
2024-10-12 11:55:44 +0200

    Add class for string_functions

--- string_functions.py
+++ string_functions.py
@@ -1,54 +1,72 @@
-# Fonctions de manipulation de chaînes de caractères
+import string
 
-def to_uppercase(s):
-    return s.upper()
+class StringManipulator:
+    """A class for performing various string manipulations."""
 
-def to_lowercase(s):
-    return s.lower()
+    def to_uppercase(self, s):
+        """Converts the input string to uppercase."""
+        return s.upper()
 
-def capitalize_first_letter(s):
-    return s.capitalize()
+    def to_lowercase(self, s):
+        """Converts the input string to lowercase."""
+        return s.lower()
 
-def reverse_string(s):
-    return s[::-1]
+    def capitalize_first_letter(self, s):
+        """Capitalizes the first letter of the input string."""
+        return s.capitalize()
 
-def count_vowels(s):
-    vowels = "aeiouAEIOU"
-    return sum(1 for char in s if char in vowels)
+    def reverse_string(self, s):
+        """Reverses the input string."""
+        return s[::-1]
 
-def count_consonants(s):
-    vowels = "aeiouAEIOU"
-    return sum(1 for char in s if char.isalpha() and char not in vowels)
+    def count_vowels(self, s):
+        """Counts the number of vowels in the input string."""
+        vowels = "aeiouAEIOU"
+        return sum(1 for char in s if char in vowels)
 
-def word_count(s):
-    return len(s.split())
+    def count_consonants(self, s):
+        """Counts the number of consonants in the input string."""
+        vowels = "aeiouAEIOU"
+        return sum(1 for char in s if char.isalpha() and char not in vowels)
 
-def is_palindrome(s):
-    cleaned = ''.join(e for e in s if e.isalnum()).lower()
-    return cleaned == cleaned[::-1]
+    def word_count(self, s):
+        """Returns the number of words in the input string."""
+        return len(s.split())
 
-def char_frequencies(s):
-    return {char: s.count(char) for char in set(s)}
+    def is_palindrome(self, s):
+        """Checks if the input string is a palindrome (ignoring case and non-alphanumeric characters)."""
+        cleaned = ''.join(e for e in s if e.isalnum()).lower()
+        return cleaned == cleaned[::-1]
 
-def find_substring(s, substring):
-    return s.find(substring)
+    def char_frequencies(self, s):
+        """Returns a dictionary of character frequencies in the input string."""
+        return {char: s.count(char) for char in set(s)}
 
-def split_by_delimiter(s, delimiter):
-    return s.split(delimiter)
+    def find_substring(self, s, substring):
+        """Finds the first occurrence of a substring in the input string."""
+        return s.find(substring)
 
-def join_list_to_string(lst, delimiter):
-    return delimiter.join(lst)
+    def split_by_delimiter(self, s, delimiter):
+        """Splits the input string by the given delimiter."""
+        return s.split(delimiter)
 
-def remove_punctuation(s):
-    import string
-    return s.translate(str.maketrans('', '', string.punctuation))
+    def join_list_to_string(self, lst, delimiter):
+        """Joins a list of strings into a single string, using the specified delimiter."""
+        return delimiter.join(lst)
 
-def truncate_string(s, length):
-    return s[:length] if len(s) > length else s
+    def remove_punctuation(self, s):
+        """Removes all punctuation characters from the input string."""
+        return s.translate(str.maketrans('', '', string.punctuation))
 
-def remove_digits(s):
-    return ''.join([i for i in s if not i.isdigit()])
+    def truncate_string(self, s, length):
+        """Truncates the input string to the specified length."""
+        return s[:length] if len(s) > length else s
 
-def get_all_links(soup):
-    return [a['href'] for a in soup.find_all('a', href=True)]
+    def remove_digits(self, s):
+        """Removes all digit characters from the input string."""
+        return ''.join([i for i in s if not i.isdigit()])
+
+    def get_all_links(self, soup):
+        """Extracts all the 'href' links from an HTML soup object."""
+        return [a['href'] for a in soup.find_all('a', href=True)]
 
