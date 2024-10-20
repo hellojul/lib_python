71b80f87076649d7dc8b1acb197a491923569f0f
hellojul <hellojulien124@gmail.com>
2024-10-20 13:15:17 +0200

    add is_prime

---
+++ Algo/is_prime.py
@@ -0,0 +1,25 @@
+def is_prime(nb):
+    """
+    Tester si un nombre est premier.
+    https://fr.wikipedia.org/wiki/Nombre_premier
+    """
+    if nb < 2:
+        return False
+
+    for d in range(2, nb):
+        if nb % d  == 0:
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+    try:
+        cmd = int(input('Enter number: '))
+
+        if is_prime(cmd):
+            print(f'\n{cmd} is prime.')
+        else:
+            print(f'\n{cmd} is not prime.')
+    except:
+        print('Error.')
