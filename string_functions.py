84423a4737945a66d31e3e3c7778cbda1289a6c3
hellojul <hellojulien124@gmail.com>
2024-10-31 14:23:50 +0100

    Deleted class, self para and convert english to french comment

--- string_functions.py
+++ string_functions.py
@@ -1,72 +1,69 @@
 import string
 
-class StringManipulator:
-    """A class for performing various string manipulations."""
+def to_uppercase(s):
+    """Convertit la chaîne d'entrée en majuscules."""
+    return s.upper()
 
-    def to_uppercase(self, s):
-        """Converts the input string to uppercase."""
-        return s.upper()
+def to_lowercase(s):
+    """Convertit la chaîne d'entrée en minuscules."""
+    return s.lower()
 
-    def to_lowercase(self, s):
-        """Converts the input string to lowercase."""
-        return s.lower()
+def capitalize_first_letter(s):
+    """Met en majuscule la première lettre de la chaîne d'entrée."""
+    return s.capitalize()
 
-    def capitalize_first_letter(self, s):
-        """Capitalizes the first letter of the input string."""
-        return s.capitalize()
+def reverse_string(s):
+    """Inverse la chaîne d'entrée."""
+    return s[::-1]
 
-    def reverse_string(self, s):
-        """Reverses the input string."""
-        return s[::-1]
+def count_vowels(s):
+    """Compte le nombre de voyelles dans la chaîne d'entrée."""
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in s if char in vowels)
 
-    def count_vowels(self, s):
-        """Counts the number of vowels in the input string."""
-        vowels = "aeiouAEIOU"
-        return sum(1 for char in s if char in vowels)
+def count_consonants(s):
+    """Compte le nombre de consonnes dans la chaîne d'entrée."""
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in s if char.isalpha() and char not in vowels)
 
-    def count_consonants(self, s):
-        """Counts the number of consonants in the input string."""
-        vowels = "aeiouAEIOU"
-        return sum(1 for char in s if char.isalpha() and char not in vowels)
+def word_count(s):
+    """Renvoie le nombre de mots dans la chaîne d'entrée."""
+    return len(s.split())
 
-    def word_count(self, s):
-        """Returns the number of words in the input string."""
-        return len(s.split())
+def is_palindrome(s):
+    """Vérifie si la chaîne d'entrée est un palindrome (en ignorant la casse et les caractères non alphanumériques)."""
+    cleaned = ''.join(e for e in s if e.isalnum()).lower()
+    return cleaned == cleaned[::-1]
 
-    def is_palindrome(self, s):
-        """Checks if the input string is a palindrome (ignoring case and non-alphanumeric characters)."""
-        cleaned = ''.join(e for e in s if e.isalnum()).lower()
-        return cleaned == cleaned[::-1]
+def char_frequencies(s):
+    """Renvoie un dictionnaire des fréquences de caractères dans la chaîne d'entrée."""
+    return {char: s.count(char) for char in set(s)}
 
-    def char_frequencies(self, s):
-        """Returns a dictionary of character frequencies in the input string."""
-        return {char: s.count(char) for char in set(s)}
+def find_substring(s, substring):
+    """Trouve la première occurrence d'une sous-chaîne dans la chaîne d'entrée."""
+    return s.find(substring)
 
-    def find_substring(self, s, substring):
-        """Finds the first occurrence of a substring in the input string."""
-        return s.find(substring)
+def split_by_delimiter(s, delimiter):
+    """Divise la chaîne d'entrée par le délimiteur donné."""
+    return s.split(delimiter)
 
-    def split_by_delimiter(self, s, delimiter):
-        """Splits the input string by the given delimiter."""
-        return s.split(delimiter)
+def join_list_to_string(lst, delimiter):
+    """Joint une liste de chaînes en une seule chaîne, en utilisant le délimiteur spécifié."""
+    return delimiter.join(lst)
 
-    def join_list_to_string(self, lst, delimiter):
-        """Joins a list of strings into a single string, using the specified delimiter."""
-        return delimiter.join(lst)
+def remove_punctuation(s):
+    """Supprime tous les caractères de ponctuation de la chaîne d'entrée."""
+    return s.translate(str.maketrans('', '', string.punctuation))
 
-    def remove_punctuation(self, s):
-        """Removes all punctuation characters from the input string."""
-        return s.translate(str.maketrans('', '', string.punctuation))
+def truncate_string(s, length):
+    """Tronque la chaîne d'entrée à la longueur spécifiée."""
+    return s[:length] if len(s) > length else s
 
-    def truncate_string(self, s, length):
-        """Truncates the input string to the specified length."""
-        return s[:length] if len(s) > length else s
+def remove_digits(s):
+    """Supprime tous les caractères numériques de la chaîne d'entrée."""
+    return ''.join([i for i in s if not i.isdigit()])
 
-    def remove_digits(self, s):
-        """Removes all digit characters from the input string."""
-        return ''.join([i for i in s if not i.isdigit()])
-
-    def get_all_links(self, soup):
-        """Extracts all the 'href' links from an HTML soup object."""
-        return [a['href'] for a in soup.find_all('a', href=True)]
+def get_all_links(soup):
+    """Extrait tous les liens 'href' d'un objet soup HTML."""
+    return [a['href'] for a in soup.find_all('a', href=True)]
 
